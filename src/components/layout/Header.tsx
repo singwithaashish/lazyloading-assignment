d241086a11550abf4a6d73df7cefd556d33fb6a3
singwithaashish <singwithaashish@gmail.com>
2023-06-24 14:27:34 +0530

    fix: made header responsive

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,14 +5,14 @@ export default function Header() {
     await signoutWithFirebase();
   };
   return (
-    <div className="flex justify-between items-center py-4 px-2 md:px-8 bg-white shadow-md sticky top-0 z-50">
+    <div className="flex justify-between gap-x-1 items-center py-4 px-2 md:px-8 bg-white shadow-md sticky top-0 z-50">
       <div className="flex items-center">
         <img
           src="https://img.icons8.com/color/48/000000/airplane-take-off.png"
           alt="logo"
           className="h-12 w-12 mr-2"
         />
-        <span className="font-semibold text-xl tracking-tight">
+        <span className="font-semibold md:text-xl tracking-tight">
           Passengers Flight Details
         </span>
       </div>
@@ -21,7 +21,7 @@ export default function Header() {
           onClick={() => window.location.href = "/reset-password"}
           className="px-4 py-2 bg-primary text-white font-semibold rounded-lg duration-200 shadow-md hover:bg-hover"
         >
-          Change Password
+          Change-pw
         </button>
         <button
           onClick={handleLogout}
